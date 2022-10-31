be5279702f936e2ff92a2554d8e86a567c7dcba5
redjizz <niko.balassis@hotmail.com>
2022-10-31 23:10:59 +0100

    Revert "Delete config.js"
    This reverts commit afde77cc92dc3207d2ea3c337d4654f915629cc9.

---
+++ config.js
@@ -0,0 +1,9 @@
+export const token = "MTAxNjczOTQzODc3ODM5MjY0OA.GkOcLA.HdJGJJwHIHNyea005AD2Dsi_pjFmpA6izpS6a0"
+export const guildId = ""
+export const clientId = "1016739438778392648"
+export const bddLogs = {
+    host: 'localhost', 
+    user:'root', 
+    password: '',
+    connectionLimit: 5
+}
